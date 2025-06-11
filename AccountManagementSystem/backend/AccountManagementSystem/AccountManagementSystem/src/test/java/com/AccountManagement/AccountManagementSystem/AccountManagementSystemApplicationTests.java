87c814e84f6a6ce53c176fa53ca179f986825d28
utkrist00-shahi <utkristshahi56@gmail.com>
2025-06-11 07:35:21 +0545

    spring boot mariadb database connection

---
+++ AccountManagementSystem/backend/AccountManagementSystem/AccountManagementSystem/src/test/java/com/AccountManagement/AccountManagementSystem/AccountManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AccountManagement.AccountManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
