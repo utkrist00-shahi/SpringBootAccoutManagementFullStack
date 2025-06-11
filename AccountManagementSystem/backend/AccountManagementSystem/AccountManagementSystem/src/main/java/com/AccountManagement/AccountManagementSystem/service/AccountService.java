adb03cdd343548ade02e5dfb16024ba50fe40539
utkrist00-shahi <utkristshahi56@gmail.com>
2025-06-11 08:48:06 +0545

    Repository applied and logic added to service class

--- AccountManagementSystem/backend/AccountManagementSystem/AccountManagementSystem/src/main/java/com/AccountManagement/AccountManagementSystem/service/AccountService.java
+++ AccountManagementSystem/backend/AccountManagementSystem/AccountManagementSystem/src/main/java/com/AccountManagement/AccountManagementSystem/service/AccountService.java
@@ -1,9 +1,25 @@
 package com.AccountManagement.AccountManagementSystem.service;
 
 import com.AccountManagement.AccountManagementSystem.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.AccountManagement.AccountManagementSystem.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import java.util.List;
 
 @Service
-public interface AccountService extends JpaRepository<Account, Long> {
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    public Account AddAccount(@RequestBody Account account) {
+        return accountRepository.save(account);
+    }
+
+
+    public List<Account> findAll() {
+        return accountRepository.findAll();
+    }
 }
